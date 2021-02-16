3fd42db2e70feb0c85000764bc9f29cc3ecc4af7
kweripx <vitor.hugoqueiroz11@gmail.com>
2021-02-16 16:52:59 -0300

    testando mudanças

--- bot.py
+++ bot.py
@@ -11,30 +11,30 @@ TOKEN= '1620239627:AAFn7BmK9KklQtTr-i7cCeKCqTSBN0FWOyI'
 
 def start(update, context):
     """Send a message when the command /start is issued."""
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Olá! Eu sou o bot PlanejamentoPET')
+    update.message.reply_text('Olá! Eu sou o bot PlanejamentoPET')
 
 def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Desculpa. Não entendi o seu comando")
+   update.message.reply_text('Desculpa. Não entendi o seu comando')
 
 unknown_handler = MessageHandler(Filters.command, unknown)
 
 def sendImage(update,context):
     url='https://i.ibb.co/j8q5J85/planej.jpg'
-    context.bot.send_photo(chat_id=update.effective_chat.id, photo=url)
+    update.message.reply_photo(chat_id=update.effective_chat.id, photo=url)
 
 def help(update,context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Esses são os comandos disponiveis. \n /help  \n /start \n /planej')
+    update.message.reply_text('Esses são os comandos disponiveis. \n /help  \n /start \n /planej')
 
-"""START THE BOT"""
+
 def main():
-    updater = Updater('1620239627:AAFn7BmK9KklQtTr-i7cCeKCqTSBN0FWOyI')
+    updater = Updater('1620239627:AAFn7BmK9KklQtTr-i7cCeKCqTSBN0FWOyI', use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("planej",sendImage))
     dp.add_handler(CommandHandler("start",start))
     dp.add_handler(CommandHandler("help",help))
     dp.add_handler(unknown_handler)
 
-
+"""START THE BOT"""
 updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
