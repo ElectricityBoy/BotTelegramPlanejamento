fc3c733b4c489894e8a93f79a982e1952675272d
Kweripx <vitor.hugoqueiroz11@gmail.com>
2021-02-15 22:53:24 -0300

    bot adicionado

---
+++ bot.py
@@ -0,0 +1,45 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+import os
+
+PORT = int(os.environ.get('PORT', 5000))
+TOKEN= '1640047450:AAFyIlPqqgvUU-CWSPLj2jeyFpzOZEkzSOM'
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Olá! Eu sou o bot PlanejamentoPET')
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Desculpa. Não entendi o seu comando")
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+
+def sendImage(update,context):
+    url='https://i.ibb.co/j8q5J85/planej.jpg'
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=url)
+
+def help(update,context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Esses são os comandos disponiveis. \n /help  \n /start \n /planej')
+
+
+def main():
+    updater = Updater('1640047450:AAFyIlPqqgvUU-CWSPLj2jeyFpzOZEkzSOM')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("planej",sendImage))
+    dp.add_handler(CommandHandler("start",start))
+    dp.add_handler(CommandHandler("help",help))
+    dp.add_handler(unknown_handler)
+
+
+updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+updater.bot.setWebhook('https://obscure-tundra-88639.herokuapp.com/' + TOKEN)
+
+updater.idle()
+
+if __name__ == '__main__':
+    main()
